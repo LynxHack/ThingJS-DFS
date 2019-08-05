76e37f92a99a2d250de191911f6be4d60e5729d3
LynxHack <KevinLynx_L@hotmail.com>
2019-08-04 18:16:20 -0700

    Added hearbeat code refactored

--- Master.js
+++ Master.js
@@ -1,23 +1,63 @@
-"use strict";
-
 var things = require('things-js');
 const uuidv1 = require('uuid/v1');
 
-export default class Master{
+class Master{
     constructor(pubsubURL){
         this.pubsub = new things.Pubsub(pubsubURL);
-        this.masterMetadataObjects = new Map();  //Maps file name/path to its master metadata
         this.dataNodes = new Map(); //Maps each data node ID to its Node metadata    
         this.initialize(this.pubsub);
+
+        this.alivelist = {} //nodeID, bool
+    }
+ 
+    initialize(){
+        return new Promise((resolve, reject) => {
+            this.pubsub.subscribe('init', (req) => {
+                var newid = uuidv1();
+                this.pubsub.publish('init', {
+                    sender: 'master',
+                    nodeID = newid,
+                    message: "Acknowledged by master"
+                });
+            }).then((topic) => { 
+                console.log(`subscribed to ${topic}`)
+            });
+        });
     }
 
-    initialize(pubsub){
-        this.pubsub.subscribe('init', (req) => {
-            console.log(req);
-            console.log("received an incoming request from a slave node");
-        });
-        // this.pubsub.subscribe('heartbeat', (req) => {
-        //     self.processHeartbeat(message)
-        // });
+
+    // Heartbeats code
+    initheartbeat(){
+        this.pubsub.subscribe('heartbeat', (req) => {
+            var nodeID = req.sender;
+            if(this.alivelist[nodeID]){
+                alivelist[nodeID] = true;
+            }
+        }).then(checknodes()); //start checking nodes once master is successfully set up with heartbeat subscription
     }
+
+    checknodes(){
+        while(true){
+            for (node in this.alivelist){ this.alivelist[node] = false; } //initiate all to false
+            this.pubsub.publish('heartbeat', {
+                sender: "master",
+                message: "Are you alive"
+            })
+
+            // Kill nodes that have not responded within 4 seconds
+            setTimeout(4000, () => {
+                var deadnodes = [];
+                for(node in this.alivelist){ 
+                    if(!this.alivelist[node]){
+                        deadnodes.push(node);
+                    }
+                }
+            });
+        }
+    }
+
 }
+
+module.exports = Master;
+
+
